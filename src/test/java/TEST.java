1435ef48d03d55432f9f4d90534f3b16f3c54d80
wangs <wangs@bsfit.com.cn>
2016-07-07 16:14:30 +0800

    ppp

--- src/test/java/TEST.java
+++ src/test/java/TEST.java
@@ -1,10 +1,9 @@
 
 public class TEST {
 	public static void main(String[] args) {
-		System.out.println("just a little test1!");	
+
 		System.out.println("just a little test2!");
 		System.out.println("just a little test3!");
 
-		System.out.println("just a little test5!");
 	}
 }

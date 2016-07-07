e5b1fc41292b70de1646f14d4b2483e4da6b5f60
wangs <wangs@bsfit.com.cn>
2016-07-07 16:02:47 +0800

    test add sysout

--- src/test/java/TEST.java
+++ src/test/java/TEST.java
@@ -3,5 +3,8 @@ public class TEST {
 	public static void main(String[] args) {
 
 		System.out.println("just a little test2!");
+		System.out.println("just a little test3!");
+		System.out.println("just a little test4!");
+		System.out.println("just a little test5!");
 	}
 }

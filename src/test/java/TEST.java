82008323a0afe2a1fbc5031530393a47471a94d8
wangs <wangs@bsfit.com.cn>
2016-07-07 16:17:00 +0800

    fdsf

--- src/test/java/TEST.java
+++ src/test/java/TEST.java
@@ -1,7 +1,7 @@
 
 public class TEST {
 	public static void main(String[] args) {
-
+		System.out.println("just a little test1!");
 		System.out.println("just a little test2!");
 		System.out.println("just a little test3!");
 

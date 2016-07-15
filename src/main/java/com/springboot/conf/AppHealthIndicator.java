a58ae1e248c1a6ccb6f14bcda5f79705d947beda
wangs <wangs@bsfit.com.cn>
2016-07-15 10:14:39 +0800

    pom文件添加产生git.properties文件的预配置插件

--- src/main/java/com/springboot/conf/AppHealthIndicator.java
+++ src/main/java/com/springboot/conf/AppHealthIndicator.java
@@ -1,16 +1,19 @@
-package com.springboot.conf;
-
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.stereotype.Component;
+//package com.springboot.conf;
+//
+//import org.springframework.boot.actuate.health.Health;
+//import org.springframework.boot.actuate.health.HealthIndicator;
+//import org.springframework.stereotype.Component;
+//
 //@Component
-public class AppHealthIndicator implements HealthIndicator{
-
-	@Override
-	public Health health() {
-		
-		return Health.up().build();
-	}
-	
-
-}
+//public class AppHealthIndicator implements HealthIndicator {
+//
+//	@Override
+//	public Health health() {
+//		int errorCode = check(); // perform some specific health check
+//		if (errorCode != 0) {
+//			return Health.down().withDetail("Error	Code", errorCode).build();
+//		}
+//		return Health.up().build();
+//	}
+//
+//}
